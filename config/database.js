bce89e36d323f7d5a527168934e86511cde010d1
daimingzhong <daimingzhong@gmail.com>
2017-10-20 15:11:26 -0400

    add config

--- config/database.js
+++ config/database.js
@@ -3,6 +3,5 @@ module.exports = {
     //'url' : 'mongodb://jeremy:123@ds037814.mongolab.com:37814/heroku_00xvwc99'
     // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
     // 'url':'mongodb://Yichen:123123@ds139847.mlab.com:39847/chip-system',
-    'url':'mongodb://Yichen:123123@ds139847.mlab.com:39847/chip-system'
-
+    'url':'mongodb://daimingzhong:123456@ds125555.mlab.com:25555/test-1'
 };
