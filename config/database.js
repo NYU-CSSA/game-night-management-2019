29958e3f8d78cb785db7f1ce6df227281aa4f84f
daimingzhong <daimingzhong@gmail.com>
2017-11-08 14:57:12 -0500

    modify db connection

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@ module.exports = {
     //'url' : 'mongodb://jeremy:123@ds037814.mongolab.com:37814/heroku_00xvwc99'
     // 'url':'mongodb://Yichen:123123@ds139847.mlab.com:39847/chip-system',
 
-    // 'url':'mongodb://daimingzhong:123456@ds125555.mlab.com:25555/test-1',
-    'url': 'mongodb://localhost:27017/cssa-game-night'
+    'url':'mongodb://daimingzhong:123456@ds125555.mlab.com:25555/test-1',
+    // 'url': 'mongodb://localhost:27017/cssa-game-night'
 };
