8dee85d5c3a38740047c478a8bfb50ce6c902a78
xiangcui <xc1008@nyu.edu>
2018-11-21 00:09:26 -0500

    remove dataabse url

--- config/database.js
+++ config/database.js
@@ -1,8 +1,3 @@
 module.exports = {
-    //'url' : 'mongodb://jeremy:123@ds037814.mongolab.com:37814/heroku_00xvwc99'
-    // 'url':'mongodb://Yichen:123123@ds139847.mlab.com:39847/chip-system',
-
-     //'url':'mongodb://daimingzhong:123456@ds125555.mlab.com:25555/test-1'
-     "url":"mongodb://jeffcx:a12345@ds147044.mlab.com:47044/nyucssa-gamenight"
-    // 'url': 'mongodb://localhost:27017/cssa-game-night'
+        "url":""
 };
